351f7510b049c86665a23be1a9bb493b5ce5a4e3
Jesse Edwards <retorts.trios.8z@icloud.com>
2025-07-26 16:22:36 -0600

    Add otel to flask app for tracing

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # k8s-nginx-demo
 K8's Starter
+
+kubectl get pods
+kubectl get services
+kubectl get namespace
+kubectl get deployments
+
+kubectl get service flask-service
+
+kubectl apply -f flask-app/flask-deployment.yaml
+
+kubectl port-forward svc/prometheus-grafana 3000:80 -n monitoring
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+kubectl port-forward svc/prometheus-kube-prometheus-prometheus 9090:9090 -n monitoring
+kubectl port-forward svc/loki 3100:3100 -n monitoring
+kubectl port-forward svc/tempo-query-frontend 3200:3200 -n monitoring
+kubectl port-forward svc/flask-service 8888:80
+
+helm repo add fluent https://fluent.github.io/helm-charts
+helm repo update
+helm install fluent-bit fluent/fluent-bit \
+  --namespace monitoring --create-namespace \
+  --set backend.type=loki \
+  --set backend.loki.host=loki.monitoring.svc \
+  --set backend.loki.port=3100 \
+  --set backend.loki.tls="off"
+
+docker images
+docker rmi <imagehash>
+docker build -t tactilevisages/flask-app-traced:latest .
+docker push tactilevisages/flask-app-traced:latest
+kubectl rollout restart deployment flask-app
+
+minikube ip
+
+kubectl delete deployment flask-app
+kubectl delete service flask-service
--- flask-app/app.py
+++ flask-app/app.py
@@ -1,9 +1,48 @@
+import logging
+import time
+
 from flask import Flask
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+logging.basicConfig(level=logging.DEBUG)
+
 app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
 
-@app.route('/')
+resource = Resource(attributes={
+    SERVICE_NAME: "flask-app"
+})
+
+trace.set_tracer_provider(TracerProvider(resource=resource))
+
+otlp_exporter = OTLPSpanExporter(  # 👈 DO NOT add 'insecure=True' here
+    endpoint="http://tempo.monitoring.svc.cluster.local:4318/v1/traces"
+)
+
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(otlp_exporter)
+)
+
+@app.route("/")
 def hello():
-    return 'Hello from Flask!'
+    return "Hello, traced world!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+@app.route("/work")
+def work():
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("db-call"):
+        time.sleep(0.2)  # simulate DB call
+    with tracer.start_as_current_span("internal-logic"):
+        time.sleep(0.1)
+    return "Did some work with nested spans!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
+
+
